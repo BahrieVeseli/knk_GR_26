f19e89f76d8d8448a8d22f5c0df628ae8426d7f4
BahrieVeseli <bahrie.veseli@student.uni-pr.edu>
2025-03-28 20:04:51 +0100

    Delete src directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.knk_2025 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.knk_2025 to javafx.fxml;
-    exports com.example.knk_2025;
-}
