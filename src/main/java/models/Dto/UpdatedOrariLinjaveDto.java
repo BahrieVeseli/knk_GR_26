211792d171e9e856332e29b704d1d33539652db2
ArxhenteKadriu <149197219+Arxhenta@users.noreply.github.com>
2025-05-14 11:15:04 +0200

    updatedd

--- src/main/java/models/Dto/UpdatedOrariLinjaveDto.java
+++ src/main/java/models/Dto/UpdatedOrariLinjaveDto.java
@@ -2,16 +2,22 @@ package models.Dto;
 
 public class UpdatedOrariLinjaveDto {
     private int orariId;
+    private String dita;
 
-    public void UpdatedOrariLinjaveDto(int orariId) {
+    public void UpdatedOrariLinjaveDto(int orariId, String dita ) {
         this.orariId = orariId;
+        this.dita = dita;
     }
 
     public int getOrariId() {
+
         return orariId;
     }
-
+    public String getDita() {
+        return dita;
+    }
     public void setOrariId(int orariId) {
         this.orariId = orariId;
     }
 }
+
