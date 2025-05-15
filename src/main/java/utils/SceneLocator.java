526cecac105259887d46b2b1caba928845d9a61b
dalina baraliu <dalina.baraliu@student.uni-pr.edu>
2025-05-15 13:37:31 +0200

    SceneLocator

---
+++ src/main/java/utils/SceneLocator.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class SceneLocator {
+    public static final String LOGIN_PAGE = "/com/example/knk_2025/login-view.fxml";
+    public static final String HOME_PAGE = "/com/example/knk_2025/dashboard.fxml";
+    public static final String TRENAT_PAGE = "/com/example/knk_2025/trenat.fxml";
+    public static final String ORARI_PAGE = "/com/example/knk_2025/orari.fxml";
+    public static final String UDHETIMET_PAGE = "/com/example/knk_2025/udhetimet.fxml";
+    public static final String DESTINACIONET_PAGE = "/com/example/knk_2025/destinacionet.fxml";
+    public static final String REZERVIMET_PAGE = "/com/example/knk_2025/rezervimet.fxml";
+    public static final String AUDIT_LOG_PAGE = "/com/example/knk_2025/audit-log.fxml";
+    public static final String CREATE_USER_PAGE = "/com/example/knk_2025/create-user.fxml";
+    public static final String PROFILE_PAGE = "/com/example/knk_2025/profile.fxml";
+}
