afe9e71182260ec33c7b93f3f439687a04ae5b9a
dalina baraliu <dalina.baraliu@student.uni-pr.edu>
2025-05-21 23:28:12 +0200

    Ndryshime

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,19 @@ module com.example.knk_2025 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
+    requires java.desktop;
 
     opens com.example.knk_2025 to javafx.fxml;
+    opens Controllers to javafx.fxml;
+
+
+    opens Services;
+    exports Services;
+    opens Repository;
+    exports Repository;
+    opens models;
+    exports models;
+
     exports com.example.knk_2025;
+    exports Controllers;
 }
